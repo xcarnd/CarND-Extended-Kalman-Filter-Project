474b5197068ba13f4a02880d9e9da42b3266262a
Chen, Zhuhui <chenzhuhui@idaoben.com>
2017-06-13 01:16:12 +0800

    Filling in CalculateRMSE and CalculateJacobian.

--- src/tools.cpp
+++ src/tools.cpp
@@ -11,15 +11,30 @@ Tools::~Tools() {}
 
 VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
                               const vector<VectorXd> &ground_truth) {
-  /**
-  TODO:
-    * Calculate the RMSE here.
-  */
+    assert(estimations.size() == ground_truth.size() && estimations.size() > 0);
+
+    VectorXd v(4);
+    v << 0, 0, 0, 0;
+
+    for (std::size_t i = 0; i < estimations.size(); ++i) {
+        VectorXd diff = estimations[i] - ground_truth[i];
+        v += diff.array() * diff.array();
+    }
+    v /= estimations.size();
+    return v.array().sqrt();
 }
 
 MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
-  /**
-  TODO:
-    * Calculate a Jacobian here.
-  */
+    double px = x_state[0];
+    double py = x_state[1];
+    double vx = x_state[2];
+    double vy = x_state[3];
+    double b = px * px + py * py;
+    double a = sqrt(b);
+    double c = b * a;
+    MatrixXd H(3, 4);
+    H << px / a, py / a, 0, 0,
+         -py / b,-px / b, 0, 0,
+         py * (vx * py - vy * px) / c, px * (vy * px - vx * py) / c, px / a, py / a;
+    return H;
 }
